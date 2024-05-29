43dead27b6af0ebb21e63087f743402c0f5255f3
barisalpaslan <brsaslan333@gmail.com>
2024-05-29 09:48:06 +0300

    İstatistik - Linq sorgular

---
+++ CoreMvcBlog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvcBlog.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
+        Context c = new Context(); 
+
+        public IViewComponentResult Invoke()
+        {
+            //Toplam Blog Sayısı
+            ViewBag.v1 = bm.GetList().Count();
+
+            ViewBag.v2 = c.Comments.Count();
+
+            ViewBag.v3 = c.Categories.Count();
+
+            ViewBag.v4 = c.Contacts.Count();
+
+            return View();
+        }
+    }
+}
---
+++ CoreMvcBlog/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvcBlog.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
+        Context c = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            //Toplam Blog Sayısı
+            ViewBag.v1 = c.Blogs.OrderByDescending(x => x.CreateDate).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+
+           
+
+            return View();
+        }
+    }
+}
